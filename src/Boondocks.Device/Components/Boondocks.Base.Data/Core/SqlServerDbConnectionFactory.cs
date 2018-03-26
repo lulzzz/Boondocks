29646b0327c313c41860a08bc40a5ea8b4ea50ea
Brian Greco <blgreco72@gmail.com>
2018-03-26 11:12:57 -0400

    Reorganized Code.

---
+++ src/Boondocks.Device/Components/Boondocks.Base.Data/Core/SqlServerDbConnectionFactory.cs
@@ -0,0 +1,14 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace Boondocks.Base.Data.Core
+{
+    //Temporary workaround. Need to implement a provider lookup:
+    // https://weblog.west-wind.com/posts/2017/Nov/27/Working-around-the-lack-of-dynamic-DbProviderFactory-loading-in-NET-Core
+
+    public class SqlServerDbConnectionFactory : IDbConnectionFactory
+    {
+        public IDbConnection Create(string connectionString) 
+            => new SqlConnection(connectionString);
+    }
+}
